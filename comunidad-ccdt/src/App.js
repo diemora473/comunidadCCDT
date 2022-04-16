cb808eed36f10a93a3a17945b98883fe6cd50076
diemora473 <diego.e.morales01@gmail.com>
2022-04-16 02:17:32 -0300

    first commit

---
+++ comunidad-ccdt/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Footers from './components/Footers/Footers';
+function App() {
+  return (
+    <div className=''>
+      <Footers />
+    </div>
+  )
+}
+
+export default App;
